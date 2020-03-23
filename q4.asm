715df681504cb59025c99932f2e5ed172a4b836a
luccamorosinigioia <luccagioia@gmail.com>
2020-03-23 13:49:39 -0300

    q4 Finalizada

--- q4.asm
+++ q4.asm
@@ -3,24 +3,24 @@ jmp 0x0000:start
 
 str1 db 'Digite o tamanho da base: ', 13, 10, 0
 str2 db 'Digite o tamanho da altura: ', 13, 10, 0
-new_line db '', 13, 10, 0
 
-width_u db 0  ;unidade
-width_d db 0  ;dezena
 width db 0
-
-height_u db 0 ;unidade
-height_d db 0 ;dezena
 height db 0
-
 aux db 0
+
 x db 1
 y db 0
 
 
 start:
+
     mov ah, 0eh
     int 10h
+
+    mov AX, 0012h           
+    mov bh, 0
+	mov bl, 2
+	int 10h
     
     call entrada
 
@@ -31,6 +31,7 @@ entrada:
     mov si, str1
     call print_str
     call recebe_w
+    
     call n_line
 
     xor ah, ah
@@ -41,116 +42,91 @@ entrada:
     call recebe_h
     call n_line
 
-    call soma
+    mov ah, 0              
+    int 10h
 
-    call loop
+    mov AX, 0012h          
+    mov bh, 0
+	mov bl, 2
+	int 10h
+
+    mov ah, 02h            
+    mov bh, 0
+    mov dh, 0
+    mov dl, 0
+    int 10h
+
+    call print_
 
     ret
 
-soma:
-    
-    call height_
-    call width_
-    ret
+print_:
 
-height_:
-    mov ax, [height_d]
-    mov cx, 10
-    mul cx
-
-    add ax, [height_u]
-    ;sub ax, 1
-    mov [height], ax
-    ret
-
-width_:
-    mov ax, [width_d]
-    mov cx, 10
-    mul cx
-
-    add ax, [width_u]
-    sub ax, 1
-    mov [height], ax
-    ret
-
-loop:
-
-    call n_line
     mov al, '*'
     call print_line
-    call n_line
 
-    call outside_loop
-
-    call n_line
-    mov al, '*'
-    call print_line
-    call n_line
-
-    ret
-
-
-
-outside_loop:
-
-    call n_line
-
-    mov cl, [x]
-    cmp cl, [height]
+    mov ah, 1
+    cmp [height], ah
     je .end
 
-    mov cl, [y]
-    add cl, 1
-    mov [x], cl
+    call loop
+    call n_line
 
-    call inside_loop
-
-    jmp outside_loop
+    mov al, '*'
+    call print_line
+    call n_line
 
     .end:
         ret
 
-inside_loop:
 
-    mov cl, [y]
-    cmp cl, [width]
-    je .end
+
+loop:
+
+    xor ah, ah
+    mov ah, 1
+    sub [height], ah
+    sub [height], ah
+
+    .loop:
+
+        xor bh, bh
+        cmp [height], bh
+        je .end
+
+        call n_line   
+        call print_m
+         
+
+        xor ah, ah
+        mov ah, 1
+        sub [height], ah
+
+        jmp .loop
     
-    call cmp
-    
-    mov ah, 0Eh
-    mov bh, 0
-    mov bl, 2
-    int 10h
-
-    mov cl, [y]
-    add cl, 1
-    mov [y], cl
-
-    jmp inside_loop
-
     .end:
-        jmp reset_y
+        ret
 
-reset_y:
-    xor cl, cl
-    mov [y], cl
-    ret
 
-cmp:
-    xor dl, dl
-    cmp dl, [y]
-    je asterisco
+print_m:
 
-    jmp espaco
+    mov al, 42           
+    mov ah, 0xe
+	int 10h
 
-asterisco:
-    mov al, '*'
-    ret
+    mov ah, 02h             
+    mov bh, 0
+    mov dh, dh
+    mov dl, [width]
+    sub dl, 1
+    int 10h
+    
+    mov al, 42             
+    mov ah, 0xe
+    int 10h
+    ret       
 
-espaco:
-    mov al, ' '
-    ret
+
 print_line:
 
     mov cl, [y]
@@ -165,6 +141,8 @@ print_line:
     mov cl, [y]
     add cl, 1
     mov [y], cl
+
+    jmp print_line
 
     .end:
         xor cl, cl
@@ -182,36 +160,8 @@ recebe_w:
     mov bl, 2
     int 10h
 
-    cmp al, 13
-    je .end
-
     sub al, '0'
-
-    call salva_w
-
-    mov ah, 1
-    add [aux], ah
-
-    jmp recebe_w
-
-    .end:
-        ret
-
-
-salva_w:
-    
-    xor cl, cl
-    cmp [aux], cl
-    je unidade_w
-
-    mov [width_d], al
-    ret 
-
-unidade_w:
-    mov [width_u], al
-    ret
-
-recebe_h:
+    mov [width], al
 
     xor ah, ah
     int 16h
@@ -225,34 +175,76 @@ recebe_h:
     je .end
 
     sub al, '0'
+    mov [aux], al
 
-    call salva_h
+    mov al, [width]
+    mov cl, 10
+    mul cl
 
-    mov ah, 1
-    add [aux], ah
+    add al, [aux]
+    mov [width], al
 
-    jmp recebe_h
-
+    xor ah, ah
+    int 16h
+    
+    mov ah, 0Eh
+    mov bh, 0
+    mov bl, 2
+    int 10h
 
     .end:
         ret
 
-salva_h:
-    
-    xor cl, cl
-    cmp [aux], cl
-    je unidade_h
+recebe_h:
 
-    mov [height_d], al
-    ret 
+    xor ah, ah
+    int 16h
     
-unidade_h:
-    mov [width_u], al
-    ret
+    mov ah, 0Eh
+    mov bh, 0
+    mov bl, 2
+    int 10h
+
+    sub al, '0'
+    mov [height], al
+
+    xor ah, ah
+    int 16h
+    
+    mov ah, 0Eh
+    mov bh, 0
+    mov bl, 2
+    int 10h
+
+    cmp al, 13
+    je .end
+
+    sub al, '0'
+    mov [aux], al
+
+    mov al, [height]
+    mov cl, 10
+    mul cl
+
+    add al, [aux]
+    mov [height], al
+
+    xor ah, ah
+    int 16h
+    
+    mov ah, 0Eh
+    mov bh, 0
+    mov bl, 2
+    int 10h
+
+    .end:
+        ret
+
 
 print_str:
     lodsb
-    cmp al, 0
+
+    cmp al, 13
     je done
 
     mov ah,0eh
@@ -262,8 +254,11 @@ print_str:
         ret
 
 n_line:
-    mov si, new_line
-    call print_str
+    mov ah, 02h            
+    mov bh, 0
+    add dh, 1
+    mov dl, 0
+    int 10h
     ret
 
 times 510 - ($-$$) db 0
